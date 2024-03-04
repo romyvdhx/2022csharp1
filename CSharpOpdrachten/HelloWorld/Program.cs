1b4ee2043ce0ee9dbbd165bb21432a70df827536
Romy <mail@romy.dev>
2024-03-04 14:22:00 +0100

    opdr 5

---
+++ CSharpOpdrachten/HelloWorld/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
